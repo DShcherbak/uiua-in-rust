f60c5eae7334aadc810176b25ad3b10b3e243bc2
DShcherbak <denys.shcherbak.official@gmail.com>
2023-12-12 10:48:31 +0200

    minor tweaks

--- src/uiua_macros.rs
+++ src/uiua_macros.rs
@@ -1,73 +1,73 @@
 
-// use crate::stack::UiuaStack;
-// use crate::elems::UiuaElements;
+use crate::stack::UiuaStack;
+use crate::elems::UiuaElements;
 
-// macro_rules! build_one_ident {
-//     (+) => {
-//         UiuaElements::Plus
-//     };
-//     (-) => {
-//         UiuaElements::Minus
-//     };
-//     (.) => {
-//         UiuaElements::Dot
-//     };
-//     (,) => {
-//         UiuaElements::Comma
-//     };
-//     (/) => {
-//         UiuaElements::Div
-//     };
-//     (*) => {
-//         UiuaElements::Mult
-//     };
-//     ($a:ident) => {
-//         ($a).convert()
-//     };
-//     ($a:expr) => {
-//         UiuaElements::Elem($a)
-//     }
-// }
+macro_rules! build_one_ident {
+    (+) => {
+        UiuaElements::Plus
+    };
+    (-) => {
+        UiuaElements::Minus
+    };
+    (.) => {
+        UiuaElements::Dot
+    };
+    (,) => {
+        UiuaElements::Comma
+    };
+    (/) => {
+        UiuaElements::Div
+    };
+    (*) => {
+        UiuaElements::Mult
+    };
+    ($a:ident) => {
+        ($a).convert()
+    };
+    ($a:expr) => {
+        UiuaElements::Elem($a)
+    }
+}
 
-// macro_rules! build_uiua_stack {
-//     // Base case: If there are no more tokens, stop the recursion.
-//     () => {{
-//         let u = UiuaStack { chars: vec![] };
-//         u
-//     }};
+macro_rules! build_uiua_stack {
+    // Base case: If there are no more tokens, stop the recursion.
+    () => {{
+        let u = UiuaStack { chars: vec![] };
+        u
+    }};
 
-//     // Match and process an identifier.
-//     ($id:ident $($rest:tt)*) => {{
-//     //  println!("Identifier: {}", stringify!($id));
-//         let c = build_one_ident!($id);
-//         let mut stack: UiuaStack = build_uiua_stack!($($rest)*);
-//         stack.chars.push(c);
-//         stack
-//     }};
+    // Match and process an identifier.
+    ($id:ident $($rest:tt)*) => {{
+    //  println!("Identifier: {}", stringify!($id));
+        let c = build_one_ident!($id);
+        let mut stack: UiuaStack = build_uiua_stack!($($rest)*);
+        stack.chars.push(c);
+        stack
+    }};
 
-//     // Match and process a special symbol.
-//     ($sym:tt $($rest:tt)*) => {{
-//     // println!("Special Symbol: <{}>", stringify!($sym));
-//         let c = build_one_ident!($sym);
-//         let mut stack: UiuaStack = build_uiua_stack!($($rest)*);
-//         stack.chars.push(c);
-//         stack
-//     }};
+    // Match and process a special symbol.
+    ($sym:tt $($rest:tt)*) => {{
+    // println!("Special Symbol: <{}>", stringify!($sym));
+        let c = build_one_ident!($sym);
+        let mut stack: UiuaStack = build_uiua_stack!($($rest)*);
+        stack.chars.push(c);
+        stack
+    }};
 
-//     ($a:tt) => {{
-//         let c = build_one_ident!($a);
-//         let mut u = UiuaStack { chars: vec![] };
-//         u.chars.push(c);
-//         u
-//     }}
-// }
+    ($a:tt) => {{
+        let c = build_one_ident!($a);
+        let mut u = UiuaStack { chars: vec![] };
+        u.chars.push(c);
+        u
+    }}
+}
 
 
-// macro_rules! uiua {
-//     ($($x:tt)+) => {{
-//         let mut stack: UiuaStack = build_uiua_stack!($($x)+);
-//         let res = stack.calc();
-//         res
-//     }}
-// }
+macro_rules! uiua {
+    ($($x:tt)+) => {{
+        let mut stack: UiuaStack = build_uiua_stack!($($x)+);
+        let res = stack.calc();
+        res
+    }}
+}
 
