b9563ef0a22ee2e52f3d014388865b70a3e5327f
DShcherbak <denys.shcherbak.official@gmail.com>
2024-03-28 14:13:33 +0200

    dyadic arithmetic working

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 pub mod elems;
 pub mod stack;
-pub mod stack_manipulation;
+pub mod stack_calc;
+
 pub mod perform;
 pub use perform::Performer;
+
+pub mod stack_manipulation;
 pub mod dyadic_arith;
-pub mod stack_calc;
+
 pub mod perform_stack_manipulation;
 pub mod perform_dyadic_arith;
+
 #[macro_use]
 pub mod uiua_macros;
 
 pub use elems::*;
 pub use stack_manipulation::*;
+pub use dyadic_arith::*;
 
 
 #[cfg(test)]
@@ -24,6 +29,53 @@ mod tests {
     fn test_uiua_macro() {
         let result = uiua!('◌':.1);
         assert_eq!(result, UiuaElements::Elem(1));
+    }
+
+    #[test]
+    fn test_uiua_add() {
+            // Basic arithmetic operations
+    let r1 = uiua!(+ 2 1);
+    println!("+ 2 1 = {:?}", r1);
+
+    let r2 = uiua!(+ 12345 54321);
+    println!("+ 12345 54321 = {:?}", r2);
+
+    // Accessing results as numbers
+    let val = uiua!(+ 12345 54321).as_num().unwrap();
+    println!("+ 12345 54321 = {:?}", val);
+
+    // Combining variables and vectors
+    let x = 1000;
+    let v = vec![1, 2, 3];
+    let r3 = uiua!(+ x v);
+    println!("+ x v (x = 1000, v = [1, 2, 3]) = {:?}", r3);
+
+    // Accessing results as vectors
+    let r4 = uiua!(+ x v).as_vec().unwrap();
+    println!("+ x v (x = 1000, v = [1, 2, 3]) = {:?}", r4);
+
+    // Complex expressions
+    let r5 = uiua!(+ - x 1 v).as_vec().unwrap();
+    println!("+ - x 1 v (x = 1000, v = [1, 2, 3]) = {:?}", r5);
+
+    let r6 = uiua!(+ - : x 1 v);
+    println!("+ - : x 1 v (x = 1000, v = [1, 2, 3]) = {:?}", r6);
+
+    // Spacing is not important as long as Rust can distinguish 
+    let r7 = uiua!(+-:x 1 v).as_vec().unwrap();
+    println!("+-:x 1 v (x = 1000, v = [1, 2, 3]) = {:?}", r7);
+
+    // Combining operations to create more complex functions
+    // *. = ^2
+    let r8 = uiua!(*. 25).as_num().unwrap();
+    println!("*. 25 = {:?}", r8);
+
+    // Handling error cases
+    let r9 = uiua!(* 25).as_err().unwrap();
+    println!("* 25 = {:?}", r9);
+
+        let result = uiua!(*+1 2 3);
+        assert_eq!(result, UiuaElements::Elem(9));
     }
 
     // Add more tests as needed...
--- src/perform_dyadic_arith.rs
+++ src/perform_dyadic_arith.rs
@@ -1,94 +1,70 @@
 use crate::{dyadic_arith::*, Performer};
 use crate::elems::UiuaElements;
 
-// pub fn get_binary(f: UiuaElements) -> (Box<dyn Fn(i32, i32) -> i32>, String) {
-//     match f {
-//         // UiuaElements::Plus => (Box::new(|x, y| x + y), "+".to_string()),
-//         // UiuaElements::Minus => (Box::new(|x, y| x - y), "-".to_string()),
-//         // UiuaElements::Mult => (Box::new(|x, y| x * y), "*".to_string()),
-//         // UiuaElements::Div => (Box::new(|x, y| x / y), "/".to_string()),
-//         _ => todo!(),
-//     }
-// }
-
-// pub fn uiua_binary(a: UiuaElements, b: UiuaElements, f: UiuaElements) -> UiuaElements {
-//     let (f, name) = get_binary(f);
-//     match (a, b) {
-//         (UiuaElements::Elem(lhs), UiuaElements::Elem(rhs)) => UiuaElements::Elem(f(lhs, rhs)),
-//         (UiuaElements::Vector(v), UiuaElements::Elem(rhs)) => {
-//             UiuaElements::Vector(v.iter().map(|x| f(*x, rhs)).collect())
-//         }
-//         (UiuaElements::Elem(lhs), UiuaElements::Vector(v)) => {
-//             UiuaElements::Vector(v.iter().map(|x| f(lhs, *x)).collect())
-//         }
-//         (UiuaElements::Vector(lhs), UiuaElements::Vector(rhs)) => {
-//             if lhs.len() != rhs.len() {
-//                 return UiuaElements::Error(
-//                     format!("Vector length mismatch: {:?} and {:?}", lhs, rhs).to_string(),
-//                 );
-//             }
-//             UiuaElements::Vector(
-//                 lhs.iter()
-//                     .zip(rhs.into_iter())
-//                     .map(|(x, y)| f(*x, y))
-//                     .collect(),
-//             )
-//         }
-//         (e1, e2) => UiuaElements::Error(format!(
-//             "Arguments of {} cannot be of types {:?} and {:?}",
-//             name, e1, e2
-//         )),
-//     }
-// }
-
-                    // UiuaElements::Plus
-                    // | UiuaElements::Minus
-                    // | UiuaElements::Mult
-                    // | UiuaElements::Div => match reverse_stack.pop() {
-                    //     Some(lhs) => match reverse_stack.pop() {
-                    //         Some(rhs) => {
-                    //             let res = uiua_binary(lhs, rhs, elem.clone());
-                    //             reverse_stack.push(res);
-                    //         }
-                    //         None => {
-                    //             return UiuaElements::Error(format!(
-                    //                 "Not enough arguments for a function {:?}",
-                    //                 elem.clone()
-                    //             ))
-                    //         }
-                    //     },
-                    //     None => {
-                    //         return UiuaElements::Error(format!(
-                    //             "Not enough arguments for a function {:?}",
-                    //             elem.clone()
-                    //         ))
-                    //     }
-                    // },
-
-fn err() -> UiuaElements {
-    UiuaElements::Error(format!("Not enough arguments for a function"))
-        // "Not enough arguments for a function" {:?}",
-        // d.clone()
-   // ))
+fn get_binary(f: &DyadicArithmetic) -> (Box<dyn Fn(i32, i32) -> i32>, String) {
+    match f {
+        DyadicArithmetic::Add => (Box::new(|x, y| x + y), "+".to_string()),
+        DyadicArithmetic::Sub => (Box::new(|x, y| x - y), "-".to_string()),
+        DyadicArithmetic::Mult => (Box::new(|x, y| x * y), "*".to_string()),
+        DyadicArithmetic::Div => (Box::new(|x, y| x / y), "/".to_string()),
+    }
 }
 
-fn get_vars_or_err(mut stack: Vec<UiuaElements>) -> Result<(UiuaElements, UiuaElements), UiuaElements> {
+fn apply_uiua_binary(a: UiuaElements, b: UiuaElements, f: &DyadicArithmetic) -> UiuaElements {
+    let (f, name) = get_binary(f);
+    match (a, b) {
+        (UiuaElements::Elem(lhs), UiuaElements::Elem(rhs)) => UiuaElements::Elem(f(lhs, rhs)),
+        (UiuaElements::Vector(v), UiuaElements::Elem(rhs)) => {
+            UiuaElements::Vector(v.iter().map(|x| f(*x, rhs)).collect())
+        }
+        (UiuaElements::Elem(lhs), UiuaElements::Vector(v)) => {
+            UiuaElements::Vector(v.iter().map(|x| f(lhs, *x)).collect())
+        }
+        (UiuaElements::Vector(lhs), UiuaElements::Vector(rhs)) => {
+            if lhs.len() != rhs.len() {
+                return UiuaElements::Error(
+                    format!("Vector length mismatch: {:?} and {:?}", lhs, rhs).to_string(),
+                );
+            }
+            UiuaElements::Vector(
+                lhs.iter()
+                    .zip(rhs.into_iter())
+                    .map(|(x, y)| f(*x, y))
+                    .collect(),
+            )
+        }
+        (e1, e2) => UiuaElements::Error(format!(
+            "Arguments of {} cannot be of types {:?} and {:?}",
+            name, e1, e2
+        )),
+    }
+}
+
+fn err(oper: &DyadicArithmetic) -> UiuaElements {
+    UiuaElements::Error(format!(
+        "Not enough arguments for a function {:?}",
+        oper
+   ))
+}
+
+fn get_vars_or_err(oper: &DyadicArithmetic, stack: &mut Vec<UiuaElements>) -> Result<(UiuaElements, UiuaElements), UiuaElements> {
     match stack.pop() {
         Some(lhs) => match stack.pop() {
             Some(rhs) => Ok((lhs, rhs)),
-            None => Err(err())
+            None => Err(err(oper))
         },
-        None => Err(err())
+        None => Err(err(oper))
     }
 }
 
 impl Performer for DyadicArithmetic {
     fn perform(&self, mut stack: Vec<UiuaElements>) -> Result<Vec<UiuaElements>, UiuaElements> {
-        
-        match &self {
-            DyadicArithmetic::Add => { return Err(UiuaElements::Error("A".to_string()))}
-            _ => {}
+        match get_vars_or_err(&self, &mut stack) {
+            Ok((lhs, rhs)) => {
+                stack.push(apply_uiua_binary(lhs, rhs, &self));
+                Ok(stack)
+            }
+            Err(e) => Err(e)
         }
-        Ok(stack)
     }
 }
